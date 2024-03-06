85b3740101a602e37a3048fec1fa25c00cf92639
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-03-06 07:00:29 +0100

    change background

--- tkinter/lesson_one.py
+++ tkinter/lesson_one.py
@@ -3,4 +3,9 @@ from tkinter import *
 window = Tk()
 window.geometry("500x500")
 window.title("Jumong first GUI lesson")
+
+# icon = PhotoImage(file="kite.jpg")
+# window.iconphoto(True, icon)
+window.config(background="black")
+
 window.mainloop()
